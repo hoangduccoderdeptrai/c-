d7e5e91892482702af86309e4b8792f5e12efd55
hoangduccoderdeptrai <127613977+hoangduccoderdeptrai@users.noreply.github.com>
2023-03-30 08:42:22 +0700

    quick_sort algorithm

--- abc/quick_sort.cpp
+++ abc/quick_sort.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
+#include<cstring>
 using namespace std;
+int partition(int arr[],int l,int r){
+	int i =l-1;
+	int pivot =arr[r];
+	for(int j =l;j<r;j++){
+		if(arr[j]<pivot){
+			i=i+1;
+			swap(arr[i],arr[j]);
+		}
+	}
+	swap(arr[i+1],arr[r]);
+	return i+1;
+}
+int quick_sort(int arr[],int l,int r){
+	if(l<=r){
+		int pivot =partition(arr,l,r);
+		quick_sort(arr,l,pivot-1);
+		quick_sort(arr,pivot+1,r);
+	}
 
+}
 int main(){
-    
-   int arr[6]={4,4,5,6,8,2};
-   vector<int>mp;
-   for(int i=0;i<6;i++){
-        mp.insert(mp.begin()+i,arr[i]);
-   }
-   
+
+	int arr[100];
+	for(int i=0;i<10;i++){
+		arr[i] =rand()%(20-1+1)+1;
+	}
+	quick_sort(arr,0,9);
+	for(int i=0;i<10;i++){
+		cout<<arr[i]<<" ";
+	}
+
 }
